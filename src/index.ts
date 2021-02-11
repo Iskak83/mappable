e37d132ae8d0b52bb39254e1771fa284858a3652
Iskak Mantyubetov <ayzik55@gmail.com>
2021-02-11 13:57:56 -0500

    added User and Company components

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import { User } from './User';
+// import { Company } from './Company';
+import { CustomMap } from './CustomMap';
+
+const user = new User();
+const map = new CustomMap('map');
+
+map.addUserMarker(user);
+
+
+
+
+// const user = new User();
+// const company = new Company();
+
+
+// console.log(user);
+// console.log(company);
+
