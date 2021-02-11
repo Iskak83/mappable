377e56cc589a7965141837daa73dbad0b7692460
Iskak Mantyubetov <ayzik55@gmail.com>
2021-02-11 18:28:24 -0500

    added marker on the map

--- dist/src.f10117fe.js
+++ dist/src.f10117fe.js
@@ -123067,6 +123067,7 @@ var User =
 /** @class */
 function () {
   function User() {
+    this.color = 'red';
     this.name = faker_1.default.name.firstName();
     this.location = {
       lat: parseFloat(faker_1.default.address.latitude()),
@@ -123102,6 +123103,7 @@ var Company =
 /** @class */
 function () {
   function Company() {
+    this.color = 'red';
     this.companyName = faker_1.default.company.companyName();
     this.catchPhrase = faker_1.default.company.catchPhrase();
     this.location = {
@@ -123208,7 +123210,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53213" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60510" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/User.ts
+++ src/User.ts
@@ -1,12 +1,13 @@
 import faker from 'faker';
-import { isNotEmittedStatement } from 'typescript';
+import { Mappable } from './CustomMap';
 
-export class User {
+export class User implements Mappable{
     name: string;
     location: {
         lat: number;
         lng: number;
     };
+    color: string = 'red';
 
     constructor() {
         this.name = faker.name.firstName();
