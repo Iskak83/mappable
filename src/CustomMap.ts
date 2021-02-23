de247515aa9295c70bd8d7b5e7913256f890cea2
Iskak Mantyubetov <ayzik55@gmail.com>
2021-02-23 16:30:36 -0500

    added marker on the map

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -8,7 +8,7 @@ export interface Mappable {
 }
 
 export class CustomMap {
-    private googleMap: google.maps.Map;
+    // private googleMap: google.maps.Map;
 
     constructor(divId: string) {
         this.googleMap = new google.maps.Map(document.getElementById(divId), {zoom: 1, center: {
