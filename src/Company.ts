732bd8ac524547133056e2d3aefdbc45851f6e51
Iskak Mantyubetov <ayzik55@gmail.com>
2021-03-03 18:33:03 -0500

    added some styling

--- src/Company.ts
+++ src/Company.ts
@@ -8,7 +8,7 @@ export class Company implements Mappable{
         lat: number;
         lng: number
     };
-    color: string = 'red';
+    // color: string = 'red';
 
     constructor() {
         this.companyName = faker.company.companyName();
